f1f0cb6e43b0be6d553b2089f5007c1b4a56c174
pablorp80 <pablorp80@gmail.com>
2016-03-26 11:50:12 -0400

    Posting grade and feedback

---
+++ lab 3/lab3.sql
@@ -0,0 +1,38 @@
+-- 1. List the ordno and dollars of all orders.
+SELECT ordno, dollars
+FROM orders;
+-- 2. List the name and city of agents named Smith.
+SELECT name, city
+FROM agents
+WHERE name = 'Smith';
+-- 3. List the pid, name, and priceUSD of products with quantity more than 208,000.
+SELECT pid, name, priceUSD
+FROM products
+WHERE quantity> 208000;
+-- 4. List the name and cities of customers in Dallas.
+SELECT name, city
+FROM customers
+WHERE city = 'Dallas';
+-- 5. List the names of agents not in New York and not in Tokyo
+SELECT name
+FROM agents
+WHERE city <> 'New York'
+AND city <> 'Tokyo';
+-- 6. List all data for products not in Dallas or Duluth that cost US$1 or more.
+SELECT *
+FROM products
+WHERE city NOT IN('Dallas','Duluth')
+AND priceUSD >= 1;
+-- 7. List all data for orders in January or March
+SELECT *
+FROM orders
+WHERE mon IN('jan','mar');
+-- 8. List all data for orders in February less than US$500.
+SELECT *
+FROM orders
+WHERE mon ='feb'
+AND dollars < 500;
+-- 9. List all orders from the customer whose cid is C005
+SELECT ordno
+FROM orders
+WHERE cid = 'C005';
